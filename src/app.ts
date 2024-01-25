1e4648f5757cd9431d9bb770128bae7eb8e41a33
bally <108769285+ballyalley-o@users.noreply.github.com>
2024-01-25 23:46:42 +1300

    events

--- src/app.ts
+++ src/app.ts
@@ -2,5 +2,7 @@ import { User } from './model'
 
 const user = new User({ name: 'myname', age: 20 })
 
-console.log(user.get('name'))
-console.log(user.get('age'))
+user.on('change', () => {})
+user.on('click', () => {})
+
+console.log(user)

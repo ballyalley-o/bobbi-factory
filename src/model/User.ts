38bb75571ca2203796b190bb8e865fe18694493b
bally <108769285+ballyalley-o@users.noreply.github.com>
2024-01-25 23:48:33 +1300

    Add event handling functionality to User class

--- src/model/User.ts
+++ src/model/User.ts
@@ -2,6 +2,8 @@ import IUser from '../interface/IUser'
 import { CallBack } from '../types'
 
 export class User {
+  events: { [key: string]: CallBack[] } = {}
+
   constructor(private data: IUser) {}
 
   get(propName: string): number | string {
@@ -12,5 +14,21 @@ export class User {
     Object.assign(this.data, update)
   }
 
-  on(eventName: string, callback: CallBack): void {}
+  on(eventName: string, callback: CallBack): void {
+    const handler = this.events[eventName] || []
+    handler.push(callback)
+    this.events[eventName] = handler
+  }
+
+  trigger(eventName: string): void {
+    const handler = this.events[eventName]
+
+    if (!handler || handler.length === 0) {
+      return
+    }
+
+    handler.forEach((cb) => {
+      cb()
+    })
+  }
 }

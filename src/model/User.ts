6ec27c32f655447848697e0e7dbd4cfe1896ed9c
bally <108769285+ballyalley-o@users.noreply.github.com>
2024-01-29 00:20:27 +1300

    Refactor User class to use Attribute and Sync classes
    This commit refactors the User class in the src/model/User.ts file to use the Attribute and Sync classes for better code organization and separation of concerns. The User class now has separate instances of Event, Sync, and Attribute classes, and the get, set, fetch, and save methods have been updated accordingly.

--- src/model/User.ts
+++ src/model/User.ts
@@ -1,16 +1,56 @@
+import { AxiosResponse } from 'axios'
 import IUser from '../interface/IUser'
 import { Event } from './Event'
+import { Attribute } from './Attribute'
+import { Sync } from './Sync'
+import { RESPONSE } from '../constant'
 
 export class User {
   public event: Event = new Event()
+  public sync: Sync<IUser> = new Sync<IUser>()
+  public attribute: Attribute<IUser>
 
-  constructor(private data: IUser) {}
+  constructor(attr: IUser) {
+    this.attribute = new Attribute<IUser>(attr)
+  }
 
-  get(propName: string): number | string {
-    return this.data[propName]
+  get on() {
+    return this.event.on
+  }
+
+  get trigger() {
+    return this.event.trigger
+  }
+
+  get get() {
+    return this.attribute.get
   }
 
   set(update: IUser): void {
-    Object.assign(this.data, update)
+    this.attribute.set(update)
+    this.event.trigger('change')
+  }
+
+  fetch(): void {
+    const id = this.get('id')
+
+    if (typeof id !== 'number') {
+      throw new Error(RESPONSE.ERROR.FETCH['message'])
+    }
+
+    this.sync.fetch(id).then((res: AxiosResponse): void => {
+      this.set(res.data)
+    })
+  }
+
+  save(): void {
+    this.sync
+      .save(this.attribute.getAll())
+      .then((res: AxiosResponse): void => {
+        this.trigger('save')
+      })
+      .catch((err: Error): void => {
+        this.trigger('error')
+      })
   }
 }

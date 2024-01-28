35dfbe6695c20651098429295edbab5992914230
bally <108769285+ballyalley-o@users.noreply.github.com>
2024-01-29 00:20:14 +1300

    Refactor event class methods to arrow functions

--- src/model/Event.ts
+++ src/model/Event.ts
@@ -3,13 +3,13 @@ import { CallBack } from '../types'
 export class Event {
   events: { [key: string]: CallBack[] } = {}
 
-  on(eventName: string, callback: CallBack): void {
+  on = (eventName: string, callback: CallBack): void => {
     const handler = this.events[eventName] || []
     handler.push(callback)
     this.events[eventName] = handler
   }
 
-  trigger(eventName: string): void {
+  trigger = (eventName: string): void => {
     const handler = this.events[eventName]
 
     if (!handler || handler.length === 0) {

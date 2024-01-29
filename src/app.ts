dbdd563507f9120213840f30a76a8aa468f63dff
bally <108769285+ballyalley-o@users.noreply.github.com>
2024-01-30 02:52:13 +1300

    Update UserForm import and add UserEdit component

--- src/app.ts
+++ src/app.ts
@@ -3,9 +3,9 @@ import { PATH } from './path'
 import { User } from './model/User'
 import { Collection } from './model/Collection'
 import IUser from './interface/IUser'
-import { UserForm } from './views'
+import { UserForm, UserEdit } from './view'
 
-// const def = { name: 'Default', age: 0 }
+const def = { name: 'Default', age: 10 }
 // const hank = { id: 1, name: 'Hank', age: 37 }
 
 // const collection = User.buildUserCollection()
@@ -14,5 +14,9 @@ import { UserForm } from './views'
 // })
 // collection.fetch()
 
-const userForm = new UserForm(document.getElementById('root')!)
-userForm.render()
+const user = User.buildUser(def)
+const rootEl = document.getElementById('root')!
+
+const userEdit = new UserEdit(rootEl, user)
+userEdit.render()
+console.log(userEdit)

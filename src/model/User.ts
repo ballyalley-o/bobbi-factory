a9fae9fdc10df789cf7816390cd5b919d5042b0a
bally <108769285+ballyalley-o@users.noreply.github.com>
2024-01-29 14:24:18 +1300

    🦄 refactor(Refactor User class to extend Model class):

--- src/model/User.ts
+++ src/model/User.ts
@@ -1,56 +1,16 @@
-import { AxiosResponse } from 'axios'
+import { Model } from './Model'
+import { RESPONSE } from '../constant'
 import IUser from '../interface/IUser'
+import { ApiSync } from './ApiSync'
 import { Event } from './Event'
 import { Attribute } from './Attribute'
-import { Sync } from './Sync'
-import { RESPONSE } from '../constant'
 
-export class User {
-  public event: Event = new Event()
-  public sync: Sync<IUser> = new Sync<IUser>()
-  public attribute: Attribute<IUser>
-
-  constructor(attr: IUser) {
-    this.attribute = new Attribute<IUser>(attr)
-  }
-
-  get on() {
-    return this.event.on
-  }
-
-  get trigger() {
-    return this.event.trigger
-  }
-
-  get get() {
-    return this.attribute.get
-  }
-
-  set(update: IUser): void {
-    this.attribute.set(update)
-    this.event.trigger('change')
-  }
-
-  fetch(): void {
-    const id = this.get('id')
-
-    if (typeof id !== 'number') {
-      throw new Error(RESPONSE.ERROR.FETCH['message'])
-    }
-
-    this.sync.fetch(id).then((res: AxiosResponse): void => {
-      this.set(res.data)
-    })
-  }
-
-  save(): void {
-    this.sync
-      .save(this.attribute.getAll())
-      .then((res: AxiosResponse): void => {
-        this.trigger('save')
-      })
-      .catch((err: Error): void => {
-        this.trigger('error')
-      })
+export class User extends Model<IUser> {
+  static buildUser(attr: IUser): User {
+    return new User(
+      new Attribute<IUser>(attr),
+      new Event(),
+      new ApiSync<IUser>()
+    )
   }
 }

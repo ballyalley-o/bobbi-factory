ad962f228cb3da8ed55cbbf716c42ea9fe20cd28
bally <108769285+ballyalley-o@users.noreply.github.com>
2024-01-29 20:54:23 +1300

    Refactor app.ts to import and use UserForm

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,18 @@
 import axios from 'axios'
 import { PATH } from './path'
 import { User } from './model/User'
+import { Collection } from './model/Collection'
+import IUser from './interface/IUser'
+import { UserForm } from './views'
 
-const def = { name: 'Default', age: 0 }
-const hank = { id: 1, name: 'Hank', age: 37 }
+// const def = { name: 'Default', age: 0 }
+// const hank = { id: 1, name: 'Hank', age: 37 }
 
-const user = User.buildUser({ id: 2 })
+// const collection = User.buildUserCollection()
+// collection.on('change', () => {
+//   console.log(collection)
+// })
+// collection.fetch()
 
-user.on('change', () => {
-  console.log(user)
-})
-
-user.fetch()
+const userForm = new UserForm(document.getElementById('root')!)
+userForm.render()

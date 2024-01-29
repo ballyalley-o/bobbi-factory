1f54cc9fe89b9053f2c67ea36b0e18413659d92b
bally <108769285+ballyalley-o@users.noreply.github.com>
2024-01-29 20:54:16 +1300

    Refactor User class and add buildUserCollection method

--- src/model/User.ts
+++ src/model/User.ts
@@ -1,9 +1,7 @@
 import { Model } from './Model'
 import { RESPONSE } from '../constant'
 import IUser from '../interface/IUser'
-import { ApiSync } from './ApiSync'
-import { Event } from './Event'
-import { Attribute } from './Attribute'
+import { ApiSync, Event, Attribute, Collection } from '../model'
 
 export class User extends Model<IUser> {
   static buildUser(attr: IUser): User {
@@ -12,5 +10,19 @@ export class User extends Model<IUser> {
       new Event(),
       new ApiSync<IUser>()
     )
+  }
+
+  static buildUserCollection(): Collection<User, IUser> {
+    return new Collection<User, IUser>((json: IUser) => User.buildUser(json))
+  }
+
+  isAdmin(): boolean {
+    if (this.get('id') === 1) {
+      console.log('User is admin')
+      return true
+    } else {
+      console.log('User is not admin')
+      return false
+    }
   }
 }

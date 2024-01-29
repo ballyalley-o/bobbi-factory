1ead7697dfffe6f2d8308ecd1d6824b0b14720a5
bally <108769285+ballyalley-o@users.noreply.github.com>
2024-01-29 20:54:02 +1300

    Add Attribute, ApiSync, and Collection to model index.ts

--- src/model/index.ts
+++ src/model/index.ts
@@ -1,2 +1,5 @@
 export { User } from './User'
 export { Event } from './Event'
+export { Attribute } from './Attribute'
+export { ApiSync } from './ApiSync'
+export { Collection } from './Collection'
